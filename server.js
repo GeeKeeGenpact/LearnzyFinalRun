a9008898e2ea5f795c9e07fb49c4293671c93b87
GenTitans <129932397+GenTitans@users.noreply.github.com>
2023-05-08 12:35:39 +0530

    added another url to cors for depl

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors=require('cors')
 
 //middleware
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000", "https://learnzy-app.onrender.com"]
 }))
 
 app.use(express.json());
